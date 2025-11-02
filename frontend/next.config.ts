d84f2b43107dc11c36c981d8555aaa7a0baf8cca
Milen Thomas <milenthomas2020@gmail.com>
2025-11-02 12:36:19 -0500

    Remove custom Nginx config file and let jwilder handle it, just rewrite urls in the frontend based on production environment (#6)

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  rewrites() {
+    return Promise.resolve([
+      {
+        source: "/api/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://localhost:8080/api/:path*"
+            : "http://backend:8080/api/:path*",
+      },
+    ]);
+  },
 };
 
 export default nextConfig;
